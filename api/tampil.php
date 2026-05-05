3b336f215ea141d980e2cbb77e85c56f9b0046a6
Afrilza.maulid <afrilza.maulid123@gmail.com>
2026-05-05 09:54:47 +0700

    Update tampil.php

--- api/tampil.php
+++ api/tampil.php
@@ -48,7 +48,7 @@
 </table>
 
 <script>
-fetch('https://tugasdatabase.vercel.app/api_mahasiswa.php')
+fetch('api_mahasiswa.php')
 .then(response => response.json())
 .then(data => {
     let output = "";
